12917fc8f9762ed6deffd4b300126674fcfcc268
Brandon Loorits <looritsbrandon@gmail.com>
2020-12-12 20:45:21 +0200

    like and dislike function added

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -32,12 +32,36 @@ router.post('/', authorize,  (request, response) => {
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+
+    let UserID = request.currentUser.id
+    let PostID = request.params.postId
+
+    try{
+        PostModel.like(UserID,PostID, (posts) => {
+        response.status(201).json()
+    })
+
+    }catch{
+        response.status(500)
+    }
+    
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
 
+    let UserId = request.currentUser.id
+    let PostId = request.params.postId
+
+    try{
+        PostModel.unlike(UserId,PostId, (posts) => {
+        response.status(200).json()
+    })
+
+    }catch{
+        response.status(500)
+    }
 });
 
 module.exports = router;
